4f5a5930023fd043dfd5d0a2f7eb9ba3bd2281aa
Samuel <samueltseng@bluetechnology.com.tw>
2020-12-17 17:17:57 +0800

    更改會員db -> mysql

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,71 +1,32 @@
-const session = require('express-session');
 const Joi = require('joi');
-const formidable = require('formidable');
-const fs = require('fs');
+const fomidable = require('formidable');
 const debug = require('debug');
+const toRegister = require('../models/memberModel');
 
-const Member = require('../models/memberModel');
-exports.get_login_page = (req,res) => {
-    res.render('member/login');
-};
-
-exports.get_register_page = (req, res) => {
-    res.render('member/register');
-};
-
-exports.post_login = async (req, res) => {
-    const form = formidable();
-    form.parse(req, (err, fields) => {
-        if (err) throw err;
-        const {error} = validateMember(fields);
-        if (error) {
-           return res.render('member/login', {member: fields, error: error.details});
-        }
-        const member = await Member.find({email: fields.email});
-        if (!member) {
-            return res.render('member/login', {error: 'This email has not been registered'});
-        }
-        if (member.password !== fields.password) {
-            return res.render('member/login', {error: 'Wrong password'});
-        }
-    });
-};
-
-exports.post_register = (req, res) => {
+exports.postRegister = function(req, res) {
     const form = formidable();
     form.parse(req, (err, fields, files) => {
-        if (err) throw err;
-        const {error} = validateMember(fields);
-        if (error) {
-            return res.render('member/register', {member: fields, error: error.details});
+        if (err){
+            debug.debug(err.message);
+            throw err;
         }
-        const member = await Member.find({email: fields.email});
-        if (member) {
-            return res.render('member/register', {error: 'This email has been registered'});
-        }
-        Member.create({
-            name: fields.name,
-            email: fields.email,
-            password: fields.password,
-            picture: toBase64(files.picture)
-        }).then(r => {
-            res.render('member/login', {member: r});
-        });
-    });
-};
-
-const memberSchema = Joi.object({
-    name: Joi.string(),
-    email: Joi.string().email().required(),
-    password: Joi.string().pattern(/^[a-z0-9A-Z]{8,20}$/).required(),
-    r_password: Joi.ref('password')
-});
-
-function validateMember(member) {
-    return memberSchema.validate(member);
+        
+    })
 }
 
-function toBase64(file) {
-    const picture = fs.readFileSync(file.path);
-    return picture.toString('base64');
+const onTime = () => {
+    const date = new Date();
+    const mm = date.getMonth() + 1;
+    const dd = date.getDate();
+    const hh = date.getHours();
+    const mi = date.getMinutes();
+    const ss = date.getSeconds();
+
+    return [date.getFullYear(), "-" +
+        (mm > 9 ? '' : '0') + mm, "-" +
+        (dd > 9 ? '' : '0') + dd, " " +
+        (hh > 9 ? '' : '0') + hh, ":" +
+        (mi > 9 ? '' : '0') + mi, ":" +
+        (ss > 9 ? '' : '0') + ss
+    ].join('');
 }
